e285bc5929ea9e1b239f5065e7279edab619151f
AMNbot <aymenouali01@gmail.com>
2022-10-18 20:22:35 -0400

    Java classes from domain model (generated by Umple)

---
+++ Backend/src/main/java/museum/Artifact.java
@@ -0,0 +1,77 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
+
+
+
+/**
+ * namespace [folder for generated code]
+ */
+// line 4 "model.ump"
+// line 96 "model.ump"
+public class Artifact
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Artifact Attributes
+  private String artID;
+  private boolean loanable;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Artifact(String aArtID, boolean aLoanable)
+  {
+    artID = aArtID;
+    loanable = aLoanable;
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setArtID(String aArtID)
+  {
+    boolean wasSet = false;
+    artID = aArtID;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setLoanable(boolean aLoanable)
+  {
+    boolean wasSet = false;
+    loanable = aLoanable;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getArtID()
+  {
+    return artID;
+  }
+
+  /**
+   * boolean loaned;
+   */
+  public boolean getLoanable()
+  {
+    return loanable;
+  }
+
+  public void delete()
+  {}
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "artID" + ":" + getArtID()+ "," +
+            "loanable" + ":" + getLoanable()+ "]";
+  }
+}
+
+
---
+++ Backend/src/main/java/museum/Room.java
@@ -0,0 +1,112 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
+
+
+
+// line 84 "model.ump"
+// line 162 "model.ump"
+public class Room
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Room Attributes
+  private String name;
+  private int capacity;
+  private Artifact artifacts;
+
+  //Room Associations
+  private Artifact artifact;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Room(String aName, int aCapacity, Artifact aArtifacts)
+  {
+    name = aName;
+    capacity = aCapacity;
+    artifacts = aArtifacts;
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setName(String aName)
+  {
+    boolean wasSet = false;
+    name = aName;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setCapacity(int aCapacity)
+  {
+    boolean wasSet = false;
+    capacity = aCapacity;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setArtifacts(Artifact aArtifacts)
+  {
+    boolean wasSet = false;
+    artifacts = aArtifacts;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public int getCapacity()
+  {
+    return capacity;
+  }
+
+  public Artifact getArtifacts()
+  {
+    return artifacts;
+  }
+  /* Code from template association_GetOne */
+  public Artifact getArtifact()
+  {
+    return artifact;
+  }
+
+  public boolean hasArtifact()
+  {
+    boolean has = artifact != null;
+    return has;
+  }
+  /* Code from template association_SetUnidirectionalOptionalOne */
+  public boolean setArtifact(Artifact aNewArtifact)
+  {
+    boolean wasSet = false;
+    artifact = aNewArtifact;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public void delete()
+  {
+    artifact = null;
+  }
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "name" + ":" + getName()+ "," +
+            "capacity" + ":" + getCapacity()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "artifacts" + "=" + (getArtifacts() != null ? !getArtifacts().equals(this)  ? getArtifacts().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
+            "  " + "artifact = "+(getArtifact()!=null?Integer.toHexString(System.identityHashCode(getArtifact())):"null");
+  }
+}
+
+
