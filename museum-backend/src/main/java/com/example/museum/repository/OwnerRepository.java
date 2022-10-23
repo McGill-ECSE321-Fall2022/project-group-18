712e215201131512696ebafe2f16bdf25117ff14
Feihong Chu <77730835+8d5b9b@users.noreply.github.com>
2022-10-23 19:25:16 -0400

    CustomerRepo and OwnerRepo extent CrudRepo<> now

--- museum-backend/src/main/java/com/example/museum/repository/OwnerRepository.java
+++ museum-backend/src/main/java/com/example/museum/repository/OwnerRepository.java
@@ -1,9 +1,10 @@
 package com.example.museum.repository;
 
 import com.example.museum.model.Owner;
+import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
 
 @Transactional
-public interface OwnerRepository extends PersonAbsRepository<Owner> {
+public interface OwnerRepository extends PersonRepository<Owner>, CrudRepository<Owner, Integer> {
 }
