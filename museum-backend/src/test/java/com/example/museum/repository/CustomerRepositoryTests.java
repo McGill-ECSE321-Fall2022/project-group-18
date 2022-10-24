a3aa75f023f167c40709a3ce2a805d0c380b4434
AMNbot <aymenouali01@gmail.com>
2022-10-23 21:14:57 -0400

    Fix customer tests

--- museum-backend/src/test/java/com/example/museum/repository/CustomerRepositoryTests.java
+++ museum-backend/src/test/java/com/example/museum/repository/CustomerRepositoryTests.java
@@ -1,6 +1,14 @@
 package com.example.museum.repository;
 
 import com.example.museum.model.Customer;
+import com.example.museum.model.Donation;
+import com.example.museum.model.Ticket;
+import com.example.museum.model.LoanRequest;
+import com.example.museum.model.LoanedArtifact;
+import com.example.museum.model.Artifact;
+
+import java.sql.Date;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +21,16 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class CustomerRepositoryTests {
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private LoanedArtifactRepository loanedArtifactRepository;
+    @Autowired
+    private ArtifactRepository artifactRepository;
+    @Autowired
+    private DonationRepository donationRepository;
+    @Autowired
+    private LoanRequestRepository loanRequestRepository;
+    @Autowired
+    private TicketRepository ticketRepository;
 
     @AfterEach
     public void clearDatabase() {
@@ -21,12 +39,57 @@ public class CustomerRepositoryTests {
 
     @Test
     public void testPersistAndLoadCustomer() {
+        // LoanedArtifact
+        int loanFee = 1000;
+        LoanedArtifact loanedArtifact = new LoanedArtifact();
+        loanedArtifact.setLoanFee(loanFee);
+        loanedArtifact = loanedArtifactRepository.save(loanedArtifact);
+        int loanedArtifactID = loanedArtifact.getArtID();
+
+        // Artifact
+        boolean loanable = true;
+        String name = "Mona Lisa";
+        Artifact.ArtType artType = Artifact.ArtType.Painting;
+        Artifact artifact = new Artifact();
+        artifact.setLoanable(loanable);
+        artifact.setName(name);
+        artifact.setType(artType);
+        artifact = artifactRepository.save(artifact);
+        int artID1 = artifact.getArtID();
+
+        // Donation
+        Donation donation = new Donation();
+        donation.setNewDonationArtifactsList();
+        donation.addDonatedArtifact(artifact);
+        donation = donationRepository.save(donation);
+        int donationID = donation.getDonationID();
+
+        // Loan Request
+        LoanRequest loanRequest = new LoanRequest();
+        loanRequest.setNewRequestedArtifactsList();
+        loanRequest.addRequestedArtifact(artifact);
+        loanRequest = loanRequestRepository.save(loanRequest);
+        int loanRequestID = loanRequest.getRequestID();
+
+        // Ticket
+        Date day = new Date(2022, 10, 13);
+        int price = 10;
+        Ticket ticket = new Ticket();
+        ticket.setDay(day);
+        ticket.setPrice(price);
+        ticket = ticketRepository.save(ticket);
+        int ticketID = ticket.getTicketID();
+
         String customerUsername = "Customer";
         String customerPassword = "password";
 
         Customer customer = new Customer();
         customer.setUsername(customerUsername);
         customer.setPassword(customerPassword);
+        customer.addLoanedArtifact(loanedArtifact);
+        customer.addCustomerDonatedArtifact(donation);
+        customer.setLoanRequest(loanRequest);
+        customer.addCustomerTicket(ticket);
 
         customer = customerRepository.save(customer);
         int customerId = customer.getAccountID();
@@ -39,5 +102,9 @@ public class CustomerRepositoryTests {
         assertEquals(customerId, customer.getAccountID());
         assertEquals(customerUsername, customer.getUsername());
         assertEquals(customerPassword, customer.getPassword());
+        assertEquals(donationID, customer.getCustomerDonatedArtifact(0));
+        assertEquals(loanedArtifactID, customer.getLoanedArtifact(0));
+        assertEquals(loanRequestID, customer.getLoanRequest());
+        assertEquals(ticketID, customer.getCustomerTicket(0));
     }
 }
