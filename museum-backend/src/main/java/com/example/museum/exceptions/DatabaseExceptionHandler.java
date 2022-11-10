717538adf62ebdbbe36841538a6cd6d0ea9543bc
andrei-sandor <97865484+andrei-sandor@users.noreply.github.com>
2022-11-09 21:53:11 -0500

    Finalized controller/dto/service for artifact. Also, created exception class.

---
+++ museum-backend/src/main/java/com/example/museum/exceptions/DatabaseExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.museum.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class DatabaseExceptionHandler {
+    @ControllerAdvice
+    public class EventRegistrationExceptionHandler extends ResponseEntityExceptionHandler {
+
+        @ExceptionHandler(DatabaseException.class)
+        public ResponseEntity<String> handleEventRegistrationException(DatabaseException ex) {
+            return new ResponseEntity<String>(ex.getMessage(), ex.getStatus());
+        }
+
+    }
+}
