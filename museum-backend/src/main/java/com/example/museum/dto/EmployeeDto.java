35fa6d065bc2ff2213821efb40c8e0f7005e018c
mohammadShaheerb <mohammad.bilal@mail.mcgill.ca>
2022-11-16 22:46:17 -0500

    Testing commit

---
+++ museum-backend/src/main/java/com/example/museum/dto/EmployeeDto.java
@@ -0,0 +1,72 @@
+package com.example.museum.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.museum.model.Employee;
+import com.example.museum.model.EmployeeHour;
+
+public class EmployeeDto {
+    private int accountID;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private List<EmployeeHourDto> employeeHours;
+
+    public EmployeeDto(Employee employee) {
+        this.accountID = employee.getAccountID();
+        this.username = employee.getUsername();
+        this.password = employee.getPassword();
+        this.firstName = employee.getFirstName();
+        this.lastName = employee.getLastName();
+
+        this.employeeHours = new ArrayList<>();
+        for (EmployeeHour eh : employee.getEmployeeHours()) {
+            this.employeeHours.add(new EmployeeHourDto(eh));
+        }
+    }
+
+    public EmployeeDto() {
+        this.employeeHours = new ArrayList<EmployeeHourDto>();
+    }
+
+    public int getAccountID() {
+        return this.accountID;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public List<EmployeeHourDto> getEmployeeHours() {
+        return this.employeeHours;
+    }
+
+    public Employee toModel() {
+        Employee employee = new Employee();
+        employee.setAccountID(this.getAccountID());
+        employee.setUsername(this.getUsername());
+        employee.setPassword(this.getPassword());
+        employee.setFirstName(this.getFirstName());
+        employee.setLastName(this.getLastName());
+        for (EmployeeHourDto eh : this.getEmployeeHours()) {
+            employee.addEmployeeHour(eh.toModel());
+        }
+
+        return employee;
+    }
+
+}
