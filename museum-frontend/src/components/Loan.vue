183c8a3fe1e7cf27384c0d89361bce25b436a403
Feihong Chu <77730835+8d5b9b@users.noreply.github.com>
2022-11-30 13:33:24 -0500

    implement view of loans

--- museum-frontend/src/components/Loan.vue
+++ museum-frontend/src/components/Loan.vue
@@ -1,14 +1,30 @@
 <template>
   <div>
-    <h1>Loan Approval</h1>
-    <b-card class="mt-3 mx-auto text-center" style="width: 40rem;" v-for="loanID in loans">
+    <h1>Loan Approval List</h1>
+    <b-card class="mt-3 mx-auto text-center" style="width: 40rem;" v-for="(loan, index) in loansDetailList" :key="loan.loanID">
       <b-card-title>
-        Loan {{ loanID }}
+        Loan {{ loan.loanID }}
       </b-card-title>
       <b-card-text>
-        An example
+        Loan Fee: {{ loan.loanFee }}
       </b-card-text>
-      <b-button href="#" variant="primary">Approve</b-button>
+      <b-card-text>
+        <b-list-group>
+          <b-list-group-item v-for="n in loansArtifactList[index].length">
+            <h4> {{ loansArtifactList[index][n-1].name }} </h4>
+            <p class="mb-1">
+              Type: {{loansArtifactList[index][n-1].type}}
+            </p>
+            <p class="mb-1">
+              Fee: {{loansArtifactList[index][n-1].loanFee}}
+            </p>
+            <p class="mb-1">
+              Status: {{ loansArtifactList[index][n-1].loaned ? 'Loaned' : 'Not Loaned' }}
+            </p>
+          </b-list-group-item>
+        </b-list-group>
+      </b-card-text>
+      <b-button variant="primary">Approve</b-button>
       <b-button variant="danger">Reject</b-button>
     </b-card>
     <b-button @click="handleLoanOutput">Button</b-button>
@@ -19,23 +35,63 @@ import axios from "axios";
 
 export default {
   mounted() {
+    // get the loan id list first
     axios.get(process.env.NODE_ENV === "development"
     ? 'http://localhost:8080/loan/customer/all' : 'production_link')
       .then(res => {
-        this.loans = Object.keys(res.data)
+        this.loansIDList = Object.keys(res.data)
       })
       .catch(e => console.log(e))
-
   },
   data() {
     return {
-      loanID: 1,
-      loans: []
+      loansIDList: [],
+      loansDetailList:[],
+      loansArtifactList:[],
+      approveActive: true
     }
   },
   methods: {
+    handleLoanArtifactDisplay: function () {
+      return
+    },
     handleLoanOutput: function () {
-      console.log(this.loans)
+      console.log(this.loansIDList)
+      console.log(this.loansDetailList)
+      console.log(this.loansDetailList[1])
+      console.log(this.loansDetailList[1].loanArtifactDetailList)
+    },
+    handleLoanInfoUpdate: async function () {
+      for (let i = 0; i < this.loansIDList.length; i++) {
+        let retrievedLoanID = this.loansIDList[i]
+        let loanArtifactIDList = []
+          await axios.get(process.env.NODE_ENV === "development" ? `http://localhost:8080/loan/${retrievedLoanID}` : 'production_link')
+          .catch(e => console.log(e))
+          .then(res => {
+            console.log(res.data)
+            this.loansDetailList.push(res.data)
+            loanArtifactIDList = res.data.loanArtifactIDList
+          })
+
+        let loanArtifactDetailList = []
+        for (let j = 0; j < loanArtifactIDList.length; j++) {
+          let loanArtifactID = loanArtifactIDList[j]
+          await axios.get(process.env.NODE_ENV === "development" ? `http://localhost:8080/artifact/${loanArtifactID}` : 'production_link')
+            .catch(e => console.log(e))
+            .then(res => {
+              console.log(res.data)
+              loanArtifactDetailList.push(res.data)
+            })
+        }
+        this.loansArtifactList.push(loanArtifactDetailList)
+        this.loansDetailList[i].loanArtifactDetailList = loanArtifactDetailList
+      }
+      // now the list
+    }
+  },
+  watch: {
+    loansIDList() {
+      this.handleLoanInfoUpdate()
     }
   }
 }
