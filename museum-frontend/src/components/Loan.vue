fcbf6d13a68704cb7205b268429e3c90a8f2e668
Feihong Chu <77730835+8d5b9b@users.noreply.github.com>
2022-12-02 01:21:36 -0500

    implement most loan functionality. need to implement page refresh

--- museum-frontend/src/components/Loan.vue
+++ museum-frontend/src/components/Loan.vue
@@ -1,40 +1,41 @@
 <template>
   <div>
     <h1>Loan Approval List</h1>
-    <h3 v-if="noLoanExist">No Loan Request</h3>
-    <b-card class="mt-3 mx-auto text-center" style="width: 40rem;" v-for="(loan, index) in loansDetailList" :key="loan.loanID">
+    <b-card class="mt-3 mx-auto text-center" style="width: 40rem;" v-if="checkLoanExist">
+      <b-card-title>
+        No Loan Request
+      </b-card-title>
+    </b-card>
+    <b-card class="mt-3 mx-auto text-center" style="width: 40rem;" v-for="loan in loansDetailList">
       <b-card-title>
         Loan {{ loan.loanID }}
       </b-card-title>
       <b-card-text>
-        Loan Status: {{ loan.loanStatus ? 'Approved' : 'Not Approved' }}
-      </b-card-text>
-      <b-card-text>
         Loan Fee: {{ loan.loanFee }}
       </b-card-text>
       <b-card-text>
-        {{ loan.loanStatus ? 'Loan Approved' : 'Loan Not Approved' }}
+        Status: {{ loan.loanStatus ? 'Approved' : 'Not Approved' }}
       </b-card-text>
       <b-card-text>
         <b-list-group>
-          <b-list-group-item v-for="n in loansArtifactList[index].length">
-            <h4> {{ loansArtifactList[index][n-1].name }} </h4>
+          <b-list-group-item v-for="loanArtifact in loan.loanArtifactList">
+            <h4> {{ loanArtifact.name }}</h4>
             <p class="mb-1">
-              Type: {{loansArtifactList[index][n-1].type}}
+              Type: {{ loanArtifact.type }}
             </p>
             <p class="mb-1">
-              Fee: {{loansArtifactList[index][n-1].loanFee}}
+              Fee: {{ loanArtifact.loanFee }}
             </p>
             <p class="mb-1">
-              Status: {{ loansArtifactList[index][n-1].loaned ? 'Loaned' : 'Not Loaned' }}
+              Status: {{ loanArtifact.loaned ? 'Loaned' : 'Not Loaned' }}
             </p>
           </b-list-group-item>
         </b-list-group>
       </b-card-text>
-      <b-button variant="primary" @click="loanApprove(loan)">Approve</b-button>
-      <b-button variant="danger">Reject</b-button>
+      <b-button v-if="approveButton(loan)" variant="primary" @click="loanApprove(loan)">Approve</b-button>
+      <b-button v-if="rejectButton(loan)" variant="danger" @click="loanReject(loan)">Reject</b-button>
+      <b-button v-else variant="primary" @click="loanReturn(loan)">Return</b-button>
     </b-card>
-    <b-button @click="handleLoanOutput">Button</b-button>
   </div>
 </template>
 <script>
@@ -47,75 +48,89 @@ export default {
     ? 'http://localhost:8080/loan/customer/all' : 'production_link')
       .then(res => {
         this.loansIDList = Object.keys(res.data)
-        this.loanCustomerIDList = Object.entries(res.data)
       })
       .catch(e => console.log(e))
   },
   data() {
     return {
-      loanCustomerIDList: [],
       loansIDList: [],
-      loansDetailList:[],
-      loansArtifactList:[],
-      approveActive: true
+      loansDetailList:[]
     }
   },
   methods: {
-    loanApprove(loan) {
-      let loanID = loan.loanID
-      axios.get(process.env.NODE_ENV === "development" ? `http://localhost:8080/loan/update/approve?loanID=${loanID}` : 'production_link')
-      this.addLoanToCustomer(loanID)
-      this.handleLoanInfoUpdate()
+    rejectButton(loan) {
+      return loan.loanStatus === false;
     },
-    addLoanToCustomer(loanID) {
-      let customerID
-      for (const [key, value] of this.loanCustomerIDList) {
-        if (key === loanID) {
-          customerID = value
-          break
+    approveButton(loan) {
+      // check if every artifact in the loan is loaned
+      for (let i = 0; i < loan.loanArtifactList.length; i++) {
+        if (loan.loanArtifactList[i].loaned === true) {
+          return false
         }
       }
+      return true
+    },
+    handleLoanIDUpdate: function () {
+      this.loansIDList = []
+      axios.get(process.env.NODE_ENV === "development"
+        ? 'http://localhost:8080/loan/customer/all' : 'production_link')
+        .then(res => {
+          this.loansIDList = Object.keys(res.data)
+        })
+        .catch(e => console.log(e))
+    },
+    loanReturn(loan) {
+      let loanID = loan.loanID
+      let customerID = loan.customerID
+      axios.get(process.env.NODE_ENV === "development" ? `http://localhost:8080/customer/${customerID}/loans/delete?loanID=${loanID}` : 'production_link')
+      axios.get(process.env.NODE_ENV === "development" ? `http://localhost:8080/loan/update/return?loanID=${loanID}` : 'production_link')
+      this.handleLoanIDUpdate()
+      this.handleLoanInfoUpdate()
+      this.approveButton()
+      this.rejectButton()
+    },
+    loanReject(loan) {
+      let loanID = loan.loanID
+      let customerID = loan.customerID
+      axios.get(process.env.NODE_ENV === "development" ? `http://localhost:8080/customer/${customerID}/loans/delete?loanID=${loanID}` : 'production_link')
+      axios.get(process.env.NODE_ENV === "development" ? `http://localhost:8080/loan/update/remove?loanID=${loanID}` : 'production_link')
+      this.handleLoanIDUpdate()
+      this.handleLoanInfoUpdate()
+      this.approveButton()
+      this.rejectButton()
+    },
+    loanApprove(loan) {
+      let loanID = loan.loanID
+      let customerID = loan.customerID
+      axios.get(process.env.NODE_ENV === "development" ? `http://localhost:8080/loan/update/approve?loanID=${loanID}` : 'production_link')
       axios.get(process.env.NODE_ENV === "development" ? `http://localhost:8080/customer/${customerID}/loans/approve?loanID=${loanID}` : 'production_link')
+      this.handleLoanInfoUpdate()
+      this.approveButton()
+      this.rejectButton()
     },
-    handleLoanOutput: function () {
-      console.log(this.loansIDList)
-      console.log(this.loansDetailList)
-      console.log(this.loansDetailList[1])
-      console.log(this.loansDetailList[1].loanArtifactDetailList)
-    },
-    handleLoanInfoUpdate: async function () {
+
+    handleLoanInfoUpdate: function () {
       this.loansDetailList = []
-      this.loansArtifactList = []
       for (let i = 0; i < this.loansIDList.length; i++) {
         let retrievedLoanID = this.loansIDList[i]
-        let loanArtifactIDList = []
-          await axios.get(process.env.NODE_ENV === "development" ? `http://localhost:8080/loan/${retrievedLoanID}` : 'production_link')
+          axios.get(process.env.NODE_ENV === "development" ? `http://localhost:8080/loan/${retrievedLoanID}` : 'production_link')
           .catch(e => console.log(e))
           .then(res => {
             console.log(res.data)
             this.loansDetailList.push(res.data)
-            loanArtifactIDList = res.data.loanArtifactIDList
           })
-
-        let loanArtifactDetailList = []
-        for (let j = 0; j < loanArtifactIDList.length; j++) {
-          let loanArtifactID = loanArtifactIDList[j]
-          await axios.get(process.env.NODE_ENV === "development" ? `http://localhost:8080/artifact/${loanArtifactID}` : 'production_link')
-            .catch(e => console.log(e))
-            .then(res => {
-              console.log(res.data)
-              loanArtifactDetailList.push(res.data)
-            })
-        }
-        this.loansArtifactList.push(loanArtifactDetailList)
-        this.loansDetailList[i].loanArtifactDetailList = loanArtifactDetailList
       }
       // now the list
     }
   },
   watch: {
-    loanCustomerIDList() {
+    loansIDList() {
       this.handleLoanInfoUpdate()
+    }
+  },
+  computed: {
+    checkLoanExist() {
+      return this.loansIDList.length === 0;
     }
   }
 }
