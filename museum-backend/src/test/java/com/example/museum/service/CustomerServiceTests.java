b36b964102d2407e437f788a17cc731455c31454
AMNbot <aymenouali01@gmail.com>
2022-12-04 20:13:48 -0500

    Fix customer service tests

--- museum-backend/src/test/java/com/example/museum/service/CustomerServiceTests.java
+++ museum-backend/src/test/java/com/example/museum/service/CustomerServiceTests.java
@@ -18,6 +18,7 @@ import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
+import com.example.museum.dto.CustomerDto;
 import com.example.museum.exceptions.DatabaseException;
 import com.example.museum.repository.ArtifactRepository;
 import com.example.museum.repository.CustomerRepository;
@@ -29,124 +30,127 @@ import com.example.museum.repository.TicketRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomerServiceTests {
-    @Mock
-    CustomerRepository customerRepository;
-    @Mock
-    EmployeeRepository employeeRepository;
-    @Mock
-    OwnerRepository ownerRepository;
-    @Mock
-    ArtifactRepository artifactRepository;
-    @Mock
-    DonationRepository donationRepository;
-    @Mock
-    LoanRepository loanRepository;
-    @Mock
-    TicketRepository ticketRepository;
+        @Mock
+        CustomerRepository customerRepository;
+        @Mock
+        EmployeeRepository employeeRepository;
+        @Mock
+        OwnerRepository ownerRepository;
+        @Mock
+        ArtifactRepository artifactRepository;
+        @Mock
+        DonationRepository donationRepository;
+        @Mock
+        LoanRepository loanRepository;
+        @Mock
+        TicketRepository ticketRepository;
 
-    @InjectMocks
-    CustomerService customerService;
-    @InjectMocks
-    EmployeeService employeeService;
-    @InjectMocks
-    OwnerService ownerService;
-    @InjectMocks
-    ArtifactService artifactService;
-    @InjectMocks
-    DonationService donationService;
-    @InjectMocks
-    LoanService loanService;
-    @InjectMocks
-    TicketService ticketService;
+        @InjectMocks
+        CustomerService customerService;
+        @InjectMocks
+        EmployeeService employeeService;
+        @InjectMocks
+        OwnerService ownerService;
+        @InjectMocks
+        ArtifactService artifactService;
+        @InjectMocks
+        DonationService donationService;
+        @InjectMocks
+        LoanService loanService;
+        @InjectMocks
+        TicketService ticketService;
 
-    @Test
-    public void testGetCustomerID() {
-        // Artifact
-        final int art1ID = 1;
-        final boolean loaned1 = true;
-        final boolean loanable1 = true;
-        final int loanFee1 = 10;
-        final String name1 = "Mona Lisa";
-        final Artifact.ArtType artType1 = Artifact.ArtType.Painting;
-        final Artifact testArtifact1 = new Artifact(art1ID, name1, artType1, loanable1, loaned1, loanFee1);
+        @Test
+        public void testGetCustomerID() {
+                // Artifact
+                final int art1ID = 1;
+                final boolean loaned1 = true;
+                final boolean loanable1 = true;
+                final int loanFee1 = 10;
+                final String name1 = "Mona Lisa";
+                final Artifact.ArtType artType1 = Artifact.ArtType.Painting;
+                final Artifact testArtifact1 = new Artifact(art1ID, name1, artType1, loanable1, loaned1, loanFee1);
 
-        final int art2ID = 2;
-        final boolean loaned2 = true;
-        final boolean loanable2 = true;
-        final int loanFee2 = 15;
-        final String name2 = "Portrait of Dr. Gachet";
-        final Artifact.ArtType artType2 = Artifact.ArtType.Painting;
-        final Artifact testArtifact2 = new Artifact(art2ID, name2, artType2, loanable2, loaned2, loanFee2);
+                final int art2ID = 2;
+                final boolean loaned2 = true;
+                final boolean loanable2 = true;
+                final int loanFee2 = 15;
+                final String name2 = "Portrait of Dr. Gachet";
+                final Artifact.ArtType artType2 = Artifact.ArtType.Painting;
+                final Artifact testArtifact2 = new Artifact(art2ID, name2, artType2, loanable2, loaned2, loanFee2);
 
-        // Donation
-        final int donationId = 3;
-        final Donation testDonation = new Donation(art2ID);
-        testDonation.addDonatedArtifact(testArtifact1);
+                // Donation
+                final int donationId = 3;
+                final Donation testDonation = new Donation(art2ID);
+                testDonation.addDonatedArtifact(testArtifact1);
 
-        // Loan
-        final int loanId = 4;
-        final int loanFee = 10;
-        final boolean approved = false;
-        final Loan testLoan = new Loan();
-        testLoan.setRequestID(loanId);
-        testLoan.setNewrequestedArtifactsList();
-        testLoan.addRequestedArtifact(testArtifact2);
-        testLoan.setLoanFee(loanFee);
-        testLoan.setApproved(approved);
+                // Loan
+                final int loanId = 4;
+                final int loanFee = 10;
+                final boolean approved = false;
+                final Loan testLoan = new Loan();
+                testLoan.setRequestID(loanId);
+                testLoan.setNewrequestedArtifactsList();
+                testLoan.addRequestedArtifact(testArtifact2);
+                testLoan.setLoanFee(loanFee);
+                testLoan.setApproved(approved);
 
-        // Ticket
-        final int ticketId = 5;
-        final Date day = new Date(2022, 10, 13);
-        final int price = 10;
-        final Ticket testTicket = new Ticket(ticketId, day, price);
+                // Ticket
+                final int ticketId = 5;
+                final Date day = new Date(2022, 10, 13);
+                final int price = 10;
+                final Ticket testTicket = new Ticket(ticketId, day, price);
 
-        // Customer
-        final int customerID = 6;
-        final String username = "customer1";
-        final String password = "password";
-        final String firstName = "First";
-        final String lastName = "Last";
-        final int credit = 5;
-        final Customer testCustomer = new Customer(customerID, username, password, firstName, lastName, credit);
-        testCustomer.setCustomerTicketsList();
-        testCustomer.setCustomerDonatedArtifactsList();
-        testCustomer.setLoansList();
+                // Customer
+                final int customerID = 6;
+                final String username = "customer1";
+                final String password = "password";
+                final String firstName = "First";
+                final String lastName = "Last";
+                final int credit = 5;
+                final Customer testCustomer = new Customer(customerID, username, password, firstName, lastName, credit);
+                testCustomer.setCustomerTicketsList();
+                testCustomer.setCustomerDonatedArtifactsList();
+                testCustomer.setLoansList();
 
-        testCustomer.addCustomerDonatedArtifact(testDonation);
-        testCustomer.addCustomerTicket(testTicket);
-        testCustomer.addLoan(testLoan);
+                testCustomer.addCustomerDonatedArtifact(testDonation);
+                testCustomer.addCustomerTicket(testTicket);
+                testCustomer.addLoan(testLoan);
 
-        when(customerRepository.findByAccountID(customerID)).thenAnswer((InvocationOnMock invocation) -> testCustomer);
+                when(customerRepository.findByAccountID(customerID))
+                                .thenAnswer((InvocationOnMock invocation) -> testCustomer);
 
-        Customer customer = customerService.getCustomerByID(customerID);
+                Customer customer = customerService.getCustomerByID(customerID);
 
-        // Testing customer
-        assertNotNull(customer);
-        assertEquals(customerID, customer.getAccountID());
-        assertEquals(username, customer.getUsername());
-        assertEquals(password, customer.getPassword());
-        assertEquals(firstName, customer.getFirstName());
-        assertEquals(lastName, customer.getLastName());
-        // Testing associations
-        assertEquals(customer.getCustomerDonatedArtifact(0).getDonationID(),
-                testCustomer.getCustomerDonatedArtifact(0).getDonationID());
-        assertEquals(customer.getLoan(0).getRequestID(), testCustomer.getLoan(0).getRequestID());
-        assertEquals(customer.getLoan(0).getRequestedArtifact(0).getArtID(),
-                testCustomer.getLoan(0).getRequestedArtifact(0).getArtID());
-        assertEquals(customer.getCustomerTicket(0).getTicketID(), testCustomer.getCustomerTicket(0).getTicketID());
-    }
+                // Testing customer
+                assertNotNull(customer);
+                assertEquals(customerID, customer.getAccountID());
+                assertEquals(username, customer.getUsername());
+                assertEquals(password, customer.getPassword());
+                assertEquals(firstName, customer.getFirstName());
+                assertEquals(lastName, customer.getLastName());
+                // Testing associations
+                assertEquals(customer.getCustomerDonatedArtifact(0).getDonationID(),
+                                testCustomer.getCustomerDonatedArtifact(0).getDonationID());
+                assertEquals(customer.getLoan(0).getRequestID(), testCustomer.getLoan(0).getRequestID());
+                assertEquals(customer.getLoan(0).getRequestedArtifact(0).getArtID(),
+                                testCustomer.getLoan(0).getRequestedArtifact(0).getArtID());
+                assertEquals(customer.getCustomerTicket(0).getTicketID(),
+                                testCustomer.getCustomerTicket(0).getTicketID());
+        }
 
-    @Test
-    void testGetCustomerByInvalidID() {
-        final int invalidID = 11111;
+        @Test
+        void testGetCustomerByInvalidID() {
+                final int invalidID = 11111;
 
-        when(customerRepository.findByAccountID(invalidID)).thenAnswer((InvocationOnMock) -> null);
+                when(customerRepository.findByAccountID(invalidID)).thenAnswer((InvocationOnMock) -> null);
 
-        DatabaseException ex = assertThrows(DatabaseException.class, () -> customerService.getCustomerByID(invalidID));
+                DatabaseException ex = assertThrows(DatabaseException.class,
+                                () -> customerService.getCustomerByID(invalidID));
 
-        assertEquals("Customer not found", ex.getMessage());
-        assertEquals(HttpStatus.NOT_FOUND, ex.getStatus());
-    }
+                assertEquals("Customer not found", ex.getMessage());
+                assertEquals(HttpStatus.NOT_FOUND, ex.getStatus());
+        }
 
     @Test
     void testCreateCustomer() {
@@ -185,7 +189,8 @@ public class CustomerServiceTests {
 
         Customer returnedCustomer = customerService.createCustomer(testCustomer);
         when(customerRepository.findByAccountID(customerID)).thenAnswer((InvocationOnMock invocation) -> returnedCustomer);
-        Customer updatedCustomer = customerService.modifyCustomerByID(customerID, "newCustomerName", "123456", "First2", "Last2");
+        CustomerDto customerDto = new CustomerDto(new Customer(customerID, "newCustomerName", "123456", "First2", "Last2", credit));
+        Customer updatedCustomer = customerService.modifyCustomerByID(customerID, customerDto);
         assertNotNull(updatedCustomer);
         assertEquals("newCustomerName", updatedCustomer.getUsername());
         assertEquals("123456", updatedCustomer.getPassword());
@@ -207,7 +212,8 @@ public class CustomerServiceTests {
 
         Customer returnedCustomer = customerService.createCustomer(testCustomer);
         when(customerRepository.findByAccountID(customerID)).thenAnswer((InvocationOnMock invocation) -> null);
-        DatabaseException ex = assertThrows(DatabaseException.class, () -> customerService.modifyCustomerByID(customerID, "newCustomerName", "123456", "First2", "Last2"));
+        CustomerDto customerDto = new CustomerDto(new Customer(customerID, "newCustomerName", "123456", "First2", "Last2", credit));
+        DatabaseException ex = assertThrows(DatabaseException.class, () -> customerService.modifyCustomerByID(customerID, customerDto));
         assertEquals(HttpStatus.NOT_FOUND, ex.getStatus());
         assertEquals("Customer not found", ex.getMessage());
     }
@@ -251,63 +257,66 @@ public class CustomerServiceTests {
         assertEquals("Customer not found", ex.getMessage());
     }
 
-    @Test
-    void testCreateConflictingUsername() {
-        final int customerID = 1;
-        final String username = "customer1";
-        final String password = "password";
-        final String firstName = "First";
-        final String lastName = "User";
-        final int credit = 5;
-        final Customer testCustomer = new Customer(customerID, username, password, firstName, lastName, credit);
-        List<Customer> customers = new ArrayList<>();
-        customers.add(testCustomer);
+        @Test
+        void testCreateConflictingUsername() {
+                final int customerID = 1;
+                final String username = "customer1";
+                final String password = "password";
+                final String firstName = "First";
+                final String lastName = "User";
+                final int credit = 5;
+                final Customer testCustomer = new Customer(customerID, username, password, firstName, lastName, credit);
+                List<Customer> customers = new ArrayList<>();
+                customers.add(testCustomer);
 
-        when(customerRepository.findAll())
-                .thenAnswer((InvocationOnMock invocation) -> customers);
+                when(customerRepository.findAll())
+                                .thenAnswer((InvocationOnMock invocation) -> customers);
 
-        final int customerID2 = 2;
-        final String username2 = "customer1";
-        final String password2 = "password";
-        final String firstName2 = "Second";
-        final String lastName2 = "User";
-        final int credit2 = 10;
-        final Customer testCustomer2 = new Customer(customerID2, username2, password2, firstName2, lastName2, credit2);
+                final int customerID2 = 2;
+                final String username2 = "customer1";
+                final String password2 = "password";
+                final String firstName2 = "Second";
+                final String lastName2 = "User";
+                final int credit2 = 10;
+                final Customer testCustomer2 = new Customer(customerID2, username2, password2, firstName2, lastName2,
+                                credit2);
 
-        Exception ex = assertThrows(DatabaseException.class, () -> customerService.createCustomer(testCustomer2));
-        verify(customerRepository, times(0)).save(testCustomer2);
-    }
+                Exception ex = assertThrows(DatabaseException.class,
+                                () -> customerService.createCustomer(testCustomer2));
+                verify(customerRepository, times(0)).save(testCustomer2);
+        }
 
-    @Test
-    void testInvalidLoginCustomer() {
-        final int customerID = 1;
-        final String username = "customer1";
-        final String password = "password";
-        final String firstName = "First";
-        final String lastName = "Last";
-        final int credit = 5;
-        final Customer testCustomer = new Customer(customerID, username, password, firstName, lastName, credit);
+        @Test
+        void testInvalidLoginCustomer() {
+                final int customerID = 1;
+                final String username = "customer1";
+                final String password = "password";
+                final String firstName = "First";
+                final String lastName = "Last";
+                final int credit = 5;
+                final Customer testCustomer = new Customer(customerID, username, password, firstName, lastName, credit);
 
-        Exception ex = assertThrows(DatabaseException.class, () -> customerService.loginCustomer(testCustomer));
-    }
+                Exception ex = assertThrows(DatabaseException.class, () -> customerService.loginCustomer(testCustomer));
+        }
 
-    @Test
-    void testCustomerLogin(){
-        final int customerID = 1;
-        final String username = "customer";
-        final String password = "password";
-        final String firstName = "First";
-        final String lastName = "Last";
-        final int credit = 10;
-        final Customer testCustomer = new Customer(customerID, username, password, firstName, lastName, credit);
-        final Customer storedCustomer = new Customer(customerID, username, password, firstName, lastName, credit);
-        List<Customer> customers = new ArrayList<>();
-        customers.add(storedCustomer);
+        @Test
+        void testCustomerLogin() {
+                final int customerID = 1;
+                final String username = "customer";
+                final String password = "password";
+                final String firstName = "First";
+                final String lastName = "Last";
+                final int credit = 10;
+                final Customer testCustomer = new Customer(customerID, username, password, firstName, lastName, credit);
+                final Customer storedCustomer = new Customer(customerID, username, password, firstName, lastName,
+                                credit);
+                List<Customer> customers = new ArrayList<>();
+                customers.add(storedCustomer);
 
-        when(customerRepository.findAll()).thenAnswer((InvocationOnMock) -> customers);
+                when(customerRepository.findAll()).thenAnswer((InvocationOnMock) -> customers);
 
-        assertDoesNotThrow(() -> customerService.loginCustomer(testCustomer));
-    }
+                assertDoesNotThrow(() -> customerService.loginCustomer(testCustomer));
+        }
 
     @Test
     void testAddLoanToCustomer() {
