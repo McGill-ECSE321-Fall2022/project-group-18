1774873dae98c567c970b17f7a597746f2bcefc5
mohammadShaheerb <mohammad.bilal@mail.mcgill.ca>
2022-12-04 19:43:45 -0500

    final changes

--- museum-backend/src/main/java/com/example/museum/service/EmployeeHourService.java
+++ museum-backend/src/main/java/com/example/museum/service/EmployeeHourService.java
@@ -1,8 +1,11 @@
 package com.example.museum.service;
 
 import com.example.museum.exceptions.DatabaseException;
+import com.example.museum.model.Employee;
 import com.example.museum.model.EmployeeHour;
 import com.example.museum.repository.EmployeeHourRepository;
+import com.example.museum.repository.EmployeeRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -23,7 +26,7 @@ public class EmployeeHourService {
 
     @Transactional
     public EmployeeHour createEmployeeHour(EmployeeHour employeeHour) {
-        checkDateConflict(employeeHour.getEmployeeHourID(), employeeHour.getDay());
+        //checkDateConflict(employeeHour.getEmployeeHourID(), employeeHour.getDay());
         employeeHour = employeeHourRepository.save(employeeHour);
         return employeeHour;
     }
@@ -50,7 +53,7 @@ public class EmployeeHourService {
 
     @Transactional
     public EmployeeHour modifyEmployeeHourById(int id, Date day, Time startTime, Time endTime){
-        checkDateConflict(id, day);
+        //checkDateConflict(id, day);
         EmployeeHour employeeHour = employeeHourRepository.findEmployeeHourByEmployeeHourID(id);
         if(employeeHour == null){
             throw new DatabaseException(HttpStatus.NOT_FOUND, "EmployeeHour not found");
@@ -63,14 +66,19 @@ public class EmployeeHourService {
     }
 
     //this might be glitched - we need to check that employee hours don't conflict for the same employee - not that they don't conflict at all...
-    //TODO: shaheer needs to fix his part
+   /* //TODO: shaheer needs to fix his part
     private void checkDateConflict(int id, Date day) throws DatabaseException{
-        Iterator<EmployeeHour> bHours = employeeHourRepository.findAll().iterator();
-        while (bHours.hasNext()) {
-            EmployeeHour bh = bHours.next();
-            if (bh.getDay().toString().equals(day.toString()) && (bh.getEmployeeHourID() != id || id == 0)) {
-                throw new DatabaseException(HttpStatus.CONFLICT, "A EmployeeHour with the given date already exists");
+        List<Employee> employees = new ArrayList<>();
+        Iterator<Employee> emp = employees.iterator();
+        Iterator<EmployeeHour> eHours = employeeHourRepository.findAll().iterator();
+        while(emp.hasNext()){
+            Employee employee = emp.next();
+            while (eHours.hasNext()) {
+                EmployeeHour eh = eHours.next();
+                if (employee.g   eh.getDay().toString().equals(day.toString()) && (eh.getEmployeeHourID() != id || id == 0)) {
+                    throw new DatabaseException(HttpStatus.CONFLICT, "A EmployeeHour with the given date already exists");
+                }
             }
-        }
     }
+}*/ 
 }
